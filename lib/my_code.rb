8729edce8f802d4f6ef91adfb20ae2469f68199a
T Benson <t.benson@live.co.uk>
2019-12-03 17:32:41 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -7,3 +7,13 @@ def map(source_array)
   end
   new_array
 end
+
+def reduce(source_array, starting_point = 0)
+  total = 0
+  i = 0
+  while i < source_array.length do
+    total += yield(source_array[i])
+    i +=1
+  end
+  total
+end
