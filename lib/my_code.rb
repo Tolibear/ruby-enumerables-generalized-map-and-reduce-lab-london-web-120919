5c1986d1f9b102e90c035646d6d5b322279d6b95
T Benson <t.benson@live.co.uk>
2019-12-03 16:53:57 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,22 @@
-# Your Code Here
+#
+# def map(source_array)
+#   yield
+#   source_array
+# end
+#
+# def map(source_array) {}
+#   yield
+#   source_array
+# end
+
+
+def map(source_array) { }
+  new_array = []
+  # i = 0
+  # while i < source_array.length do
+  #   new_array << ( source_array[i] * -1 )
+  #   i += 1
+  # end
+  yield
+  new_array
+end
